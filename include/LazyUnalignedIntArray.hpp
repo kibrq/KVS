02183a3a09b6ec879473c671d6071c1b9b135c09
Roman Venediktov <e2e4b6b7@gmail.com>
2021-06-13 03:02:43 +0300

    Fix unaligned int array

--- include/LazyUnalignedIntArray.hpp
+++ include/LazyUnalignedIntArray.hpp
@@ -8,13 +8,36 @@
 template<size_t bits>
 class LazyUnalignedArray<LimitedUnsignedInt<bits>> {
 public:
-    constexpr static size_t bit_per_value = sizeof(unsigned int) * 8;
+    constexpr static size_t bit_per_value = bits;
 
     static unsigned int get(const char *src, size_t index) {
-        return reinterpret_cast<const unsigned int *>(src)[index];
+        size_t pos = bits * (index + 1) - 1;
+        unsigned int sum = 0;
+        for (size_t i = 0; i < bits; ++i) {
+            if (get_bit(src, pos - i)) {
+                sum += 1u << i;
+            }
+        }
+        return sum;
     }
 
     static void set(char *dst, unsigned int value, size_t index) {
-        reinterpret_cast<unsigned int *>(dst)[index] = value;
+        size_t pos = bits * (index + 1) - 1;
+        for (size_t i = 0; i < bits; ++i) {
+            set_bit(dst, pos - i, value & (1u << i));
+        }
+    }
+
+private:
+    static bool get_bit(const char *src, size_t bit) {
+        return (static_cast<unsigned int>(src[bit / 8]) & (1u << (7 - bit % 8)));
+    }
+
+    static void set_bit(char *dst, size_t bit, bool value) {
+        if (value) {
+            dst[bit / 8] = static_cast<char>(static_cast<unsigned int>(dst[bit / 8]) | (1u << (7 - bit % 8)));
+        } else {
+            dst[bit / 8] = static_cast<char>(static_cast<unsigned int>(dst[bit / 8]) & (~(1u << (7 - bit % 8))));
+        }
     }
 };
---
+++ test/testLazyUnalignedIntArray.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+#include "LazyUnalignedIntArray.hpp"
+
+template<size_t size>
+using IntArray = LazyUnalignedArray<LimitedUnsignedInt<size>>;
+
+TEST(LazyUnalignedIntArray, Simple) {
+    char data[3];
+    unsigned int values[5]{1, 2, 15, 0, 6};
+    for (size_t i = 0; i < 5; ++i) {
+        IntArray<4>::set(data, values[i], i);
+    }
+    for (size_t i = 0; i < 5; ++i) {
+        ASSERT_EQ(values[i], IntArray<4>::get(data, i));
+    }
+}
+
+TEST(LazyUnalignedIntArray, Simple17) {
+    char data[17];
+    unsigned int values[8]{1, 2, 15, 0, 6, (1u << 17) - 1, 32456, 99999};
+    for (size_t i = 0; i < 8; ++i) {
+        IntArray<17>::set(data, values[i], i);
+    }
+    for (size_t i = 0; i < 8; ++i) {
+        ASSERT_EQ(values[i], IntArray<17>::get(data, i));
+    }
+}
